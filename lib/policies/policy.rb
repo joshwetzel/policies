b30de5754bdb65e21fa07414a55119b5390275dd
Josh Wetzel <wetzelj@live.com>
2015-04-26 15:15:29 -0400

    Change user to current_user and role to current_role

--- lib/policies/policy.rb
+++ lib/policies/policy.rb
@@ -1,9 +1,9 @@
 class Policy
-  attr_reader :user, :role
+  attr_reader :current_user, :current_role
 
-  def initialize(user, role, object)
-    @user = user
-    @role = role
+  def initialize(current_user, current_role, object)
+    @current_user = current_user
+    @current_role = current_role
 
     unless object.is_a?(Symbol)
       instance_variable_set('@' + object.class.to_s.underscore, object)
